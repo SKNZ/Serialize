b7a29b2beacc4ab2ab7d5d0262c841f69e67599a
sknz <florandara@gmail.com>
2015-03-10 12:14:57 +0100

    wip

---
+++ api/api/controllers/SubscriptionsController.js
@@ -0,0 +1,11 @@
+/**
+ * SubscriptionsController
+ *
+ * @description :: Server-side logic for managing subscriptions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/api/models/Subscription.js
@@ -0,0 +1,20 @@
+/**
+* Subscriptions.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    show: {
+        model: 'show'
+    },
+    user: {
+        model: 'user'
+    },
+    notification: 'string'
+  }
+};
+
