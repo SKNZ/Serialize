0555d9c395faf8a9f8691b645a1974114aa316c2
sknz <florandara@gmail.com>
2015-03-17 18:00:47 +0100

    navbar fix

--- pc/js/DomInteraction.js
+++ pc/js/DomInteraction.js
@@ -8,45 +8,50 @@ var DomInteraction = (function () {
                     .addClass('form-group')
                     .append(
                     $('<input>')
-                        .prop('type', type)
-                        .prop('name', name)
-                        .prop('placeholder', placeholder)
+                        .attr('type', type)
+                        .attr('name', name)
+                        .attr('placeholder', placeholder)
                         .addClass('form-control'));
             };
 
             var signInButton = $('<button>')
-                .prop('type', 'submit')
-                .prop('class', 'btn btn-success')
+                .attr('type', 'submit')
+                .attr('class', 'btn btn-success')
                 .html('Sign in'); // @todo Replace html() with text()
 
             var registerButton = $('<button>')
-                .prop('class', 'btn')
+                .attr('class', 'btn')
                 .addClass('button-register')
                 .html('Register'); // @todo Replace html() with text()
 
-            return $('<form>')
-                .prop('id', 'navbar-right')
-                .addClass('navbar-form')
-                .addClass('navbar-right')
-                .append(_makeInputField('text', 'email', 'Email'))
-                .append('\n')
-                .append(_makeInputField('password', 'password', 'Password'))
-                .append('\n')
-                .append(signInButton)
-                .append('\n')
-                .append(registerButton)
-                .append('\n')
-                .submit(
-                function (event) {
-                    ApiProvider.tryLogin({
-                        email: $('#navbar-right input[name="email"]').val(),
-                        password: $('#navbar-right input[name="password"]').val()
-                    }).fail(function (errorMessages) {
+            $("#navbar").append(
+                $('<form>')
+                    .attr('id', 'navbar-right')
+                    .addClass('navbar-form')
+                    .addClass('form-inline')
+                    .addClass('navbar-right')
+                    .append(
+                        _makeInputField('text', 'email', 'Email'),
+                        '\n',
+                        _makeInputField('password', 'password', 'Password'),
+                        '\n',
+                        signInButton,
+                        '\n',
+                        registerButton,
+                        'n')
+                    .submit(
+                    function (event) {
+                        ApiProvider.tryLogin({
+                            email:
+                                $('#navbar-right input[name="email"]').val(),
+                            password:
+                                $('#navbar-right input[name="password"]').val()
+                        }).fail(function (errorMessages) {
 
-                    }).always(_updateLoginStatus);
+                        }).always(_updateLoginStatus);
 
-                    event.preventDefault();
-                });
+                        event.preventDefault();
+                    }).fadeIn('slow'));
         };
 
         var _buildProfileBar = function () {
@@ -60,15 +65,15 @@ var DomInteraction = (function () {
                         .addClass('glyphicon')
                         .addClass('glyphicon-' + glyphicon)
                         .append($('<div>')
-                                    .addClass('navbar-collapsed-right-item')
-                                    .addClass('hidden-sm hidden-md hidden-lg')
-                                    .text(text)));
+                            .addClass('navbar-collapsed-right-item')
+                            .addClass('hidden-sm hidden-md hidden-lg')
+                            .text(text)));
             }
 
             $("#navbar").append(
                 $('<ul>')
                     .hide()
-                    .prop('id', 'navbar-right')
+                    .attr('id', 'navbar-right')
                     .addClass('nav')
                     .addClass('navbar-nav')
                     .addClass('navbar-right')
@@ -85,9 +90,9 @@ var DomInteraction = (function () {
                                 .addClass('img-responsive')
                                 .addClass('img-circle')
                                 .addClass('navbar-gravatar')
-                                .prop('src', 'http://www.gravatar.com/avatar/' +
-                                             currentUser.emailHash +
-                                             '.jpg?s=19')
+                                .attr('src', 'http://www.gravatar.com/avatar/' +
+                                currentUser.emailHash +
+                                '.jpg?s=35')
                                 .load(
                                 // We wait for the animation to be loaded,
                                 // so the effect looks better
@@ -97,28 +102,44 @@ var DomInteraction = (function () {
         };
 
         var _buildSearchForm = function () {
+
         };
 
         var _buildRegisterNowButton = function () {
+            $("#text-welcome").remove();
 
+            $("block-welcome-register")
+                .append(
+                $("<span>")
+                    .attr()
+                    .append(
+                    $("<button>")
+                        .attr("type", "button")
+                        .addClass("button-register")
+                        .addClass("btn")
+                        .addClass("btn-primary")
+                        .addClass("center-block")
+                        .text("Register now !")
+                ));
         };
 
         return function () {
             var loggedIn = ApiProvider.isLoggedIn();
             // Build right side of navbar
-            loggedIn ? _buildProfileBar() : _buildLoginForm();
+            loggedIn ? _buildLoginForm() : _buildProfileBar();
             loggedIn ? _buildSearchForm() : _buildRegisterNowButton();
         };
     })();
 
     return {
         initializeDom: function () {
-            $('#page-home').show('slide', 1000, function () {
+            $('#page-home').show('slide', 'slow', function () {
                 $('#main-title').animate({
                     marginTop: '0px',
                     marginBottom: '20.100px'
-                }, 1000, function () {
-                    $('#text-welcome').removeClass('invisible').hide().show('fade', 1000);
+                }, 'slow', function () {
+                    $('#text-welcome').removeClass('invisible').hide().show('fade',
+                        'slow');
                     _updateLoginStatus();
                 });
             });
