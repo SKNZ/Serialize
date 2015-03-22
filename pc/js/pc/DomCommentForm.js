5b1674b566229558bbccd1d740263f5f4be44113
sknz <florandara@gmail.com>
2015-03-22 22:53:35 +0100

    yuolo

--- pc/js/pc/DomCommentForm.js
+++ pc/js/pc/DomCommentForm.js
@@ -1,6 +1,11 @@
 var DomCommentForm = (function () {
     return {
         initialize: function () {
+            // Autofocus on subject field when modal opens
+            $('#comment-modal').on('shown.bs.modal', function () {
+                $('#comment-input-subject').focus();
+            });
+
             $('.comment-input-rating-star').click(function () {
                 var selectedStars = $(this)
                     .prevAll('.comment-input-rating-star')
