f4315349cad58992b9b414c50cdc49ee09cf95b0
sknz <florandara@gmail.com>
2015-04-06 17:41:37 +0200

    Fix

--- js/mobile/DomInteraction.js
+++ js/mobile/DomInteraction.js
@@ -1,6 +1,12 @@
 var DomInteraction = (function () {
     return {
         initialize: function () {
+            $('#footer-to-top')
+                .click(function (event) {
+                    $('html, body').animate({scrollTop: 0}, 'slow');
+                    event.preventDefault();
+                });
+
             DomLogin.initialize();
             DomComment.initialize();
             DomShow.initialize();
